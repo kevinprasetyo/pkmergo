6bf3fa5aa11cc397d9f91eb1c5d5df82d8b05cae
Kevin Prasetyo <kevinprasetyokevin@gmail.com>
2023-09-15 06:17:09 +0700

    webcam

--- apps/cv/routes.py
+++ apps/cv/routes.py
@@ -1,22 +1,38 @@
 from apps.cv import blueprint
-from flask import render_template, request, flash, redirect
+from flask import render_template, request, flash, redirect, Response
 from flask_login import login_required, current_user
 from jinja2 import TemplateNotFound
 from apps import db, login_manager
-
-@blueprint.route('/cv')
-@login_required
-def cv():
-    return "Hello World"
+import cv2
 
 @blueprint.route('/sit')
 @login_required
 def sit():
-    return render_template('cv/sit.html', segment='cv')
+    return render_template('cv/sit.html', segment='sit')
+
+camera = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@blueprint.route('/samping')
+def samping():
+    return render_template('cv/samping.html', segment='samping')
+
+@blueprint.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # Errors
-
-
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     return render_template('home/page-403.html'), 403
